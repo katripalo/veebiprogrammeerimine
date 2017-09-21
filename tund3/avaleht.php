bf7d6214cd92d9f6fc93570b3e9be5ccff3a5918
Katri Palo <katripalo@users.noreply.github.com>
2017-09-21 12:01:44 +0300

    uuendatud

---
+++ tund3/avaleht.php
@@ -0,0 +1,28 @@
+<?php
+	//muutujad
+	$myName = "Andrus";
+	$myFamilyName = "Rinde";
+	
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		Andrus Rinde programmeerib veebi
+	</title>
+</head>
+<body>
+	<h1><?php echo $myName ." " .$myFamilyName; ?>, veebiprogrammeerimine</h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<p>Meie õpime <a href="http://www.tlu.ee">Tallinna Ülikoolis</a>.</p>
+	<p>Minu esimene php leht on <a href="../esimene.php">siin </a>.</p>
+	<P>Minu sõbra Meelise leht on <a href="../../../~lutsmeel/veebiprogrammeerimine">siin</a></p>
+	<p>Pilte näeb siin <a href="foto.php"> siin </a>.<p/>
+</body>
+</html>
+
+
+
+
+
